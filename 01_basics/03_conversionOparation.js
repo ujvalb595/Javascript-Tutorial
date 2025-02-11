d9d396094db9451ab846d6de974ae3a85f5bbf58
Ujval Bhatt <ujvalb595@gmail.com>
2025-02-11 09:38:27 +0000

    CoversionOparation

---
+++ 01_basics/03_conversionOparation.js
@@ -0,0 +1,9 @@
+let score = null
+let valueInNumber = Number(score);
+
+//console.log(typeof score);
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
