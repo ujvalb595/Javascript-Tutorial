4a9ce5edc804059e294507e59912fdbf31a489b8
Ujval Bhatt <ujvalb595@gmail.com>
2025-02-11 09:23:24 +0000

    Variables and DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1455
+let accountEmail = "ujvalb595@gmail.com"
+var accountPassword = "123456"
+accountCity = "Vadoddara"
+let accountState;
+
+// accountId = 2 // Not allowed
+
+accountEmail = "ujval@gmail.com"
+accountPassword = "456789"
+accountCity = "Surat"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; // Treat all js code as newer version
+
+// alert( 3 + 3 ) // We are using node JS not a browser
+
+console.log(3+3);
+console.log("age");
+
+let name = "Ujval"
+let age = "33"
+
+console.log(typeof age);
